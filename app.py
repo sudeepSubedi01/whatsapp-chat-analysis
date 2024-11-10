5f55b435dfce10407c338b3cc4e1fc8738906996
Sudip Subedi <sudip.subedi99@gmail.com>
2024-11-10 23:50:50 +0545

    word cloud and most common words

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+
+import pandas as pd
+from collections import Counter
 import streamlit as st
+import matplotlib.pyplot as plt
 import preprocessor
 import helper
 
@@ -22,8 +26,7 @@ if uploaded_file is not None:
 
   # BUTTON
   if st.sidebar.button('Show Analysis'):
-    num_messages,num_words,num_media,num_links = helper.fetch_stats(selected_user,df)
-    
+    num_messages,num_words,num_media,num_links = helper.fetch_stats(selected_user,df)        # DISPLAYING STATS
     col1,col2,col3,col4 = st.columns(4)
     with col1:
       st.title(num_messages)
@@ -33,9 +36,33 @@ if uploaded_file is not None:
       st.header('Total Words')
     with col3:
       st.title(num_media)
-      st.header('Total Media')
+      st.header('Media Shared')
     with col4:
       st.title(num_links)
-      st.header('Total Links')
+      st.header('Links Shared')
 
+    # MOST BUSY PERSONS
+    if selected_user == 'Overall':
+      st.title('Most Busy persons')
+      col1,col2 = st.columns(2, gap='large')
+      with col1:
+        user_per = helper.most_busy_users(df)
+        st.bar_chart(user_per,x='Person',y='Percentage', color='#ffaa00')
+      with col2:
+        st.dataframe(user_per)
 
+    # WORD CLOUD
+    st.title('Word Cloud')
+    wc_obj = helper.create_wordcloud(selected_user,df)
+    fig,ax=plt.subplots(figsize=(8,8))
+    ax.axis('off')
+    ax.imshow(wc_obj)
+    st.pyplot(fig)
+
+    # MOST COMMON WORDS
+    st.title('Most Common Words')
+    most_common_df = helper.most_common_words(selected_user,df)
+    # st.dataframe(most_common_df)
+    fig,ax = plt.subplots()
+    ax.barh(most_common_df[0], most_common_df[1])
+    st.pyplot(fig)
--- helper.py
+++ helper.py
@@ -1,19 +1,56 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 from urlextract import URLExtract
+from collections import Counter
+import streamlit as st
 ext = URLExtract()
 def fetch_stats(selected_user,df):
   if selected_user =='Overall':
-    num_messages = df.shape[0]                                                      # TOTAL MESSAGES
-    num_words = len((" ".join((df['message'].values.tolist()))).split())            # TOTAL WORDS
-    num_media = df[df['message'] == '<Media omitted>\n'].shape[0]
-    num_links = len(ext.find_urls(' '.join(df['message'].values.tolist())))
+    num_messages = df.shape[0]                                                          # TOTAL MESSAGES
+    num_words = len((" ".join((df['message'].values.tolist()))).split())                # TOTAL WORDS
+    num_media = df[df['message'] == '<Media omitted>\n'].shape[0]                       # TOTAL MEDIA
+    num_links = len(ext.find_urls(' '.join(df['message'].values.tolist())))             # TOTAL LINKS
   else:
-    num_messages = df[df['user'] == selected_user].shape[0]                         # TOTAL MESSAGES
-    new_df = df[df['user'] == df['user'][3]]                                        # TOTAL WORDS
+    num_messages = df[df['user'] == selected_user].shape[0]                             # TOTAL MESSAGES
+    new_df = df[df['user'] == selected_user]                                            # TOTAL WORDS
     num_words = len((' '.join(new_df['message'].values.tolist())).split())
-    num_media = new_df[new_df['message'] == '<Media omitted>\n'].shape[0]
-    num_links = len(ext.find_urls(' '.join(new_df['message'].values.tolist())))
+    num_media = new_df[new_df['message'] == '<Media omitted>\n'].shape[0]               # TOTAL MEDIA
+    num_links = len(ext.find_urls(' '.join(new_df['message'].values.tolist())))         # TOTAL LINKS
 
   
   return num_messages,num_words, num_media, num_links
 
-  # TOTAL WORDS
+
+def most_busy_users(df):
+  user_per = round((df['user'].value_counts().head() / df['user'].shape[0] )*100,2)
+  user_per = user_per.reset_index().rename(columns={'user':'Person','count':'Percentage'})
+  return user_per
+
+
+def create_wordcloud(selected_user,df):
+  if selected_user != 'Overall':
+    df = df[df['user'] == selected_user]
+  wc= WordCloud(width=500, height=500,min_font_size=10, background_color='white')
+  wc_obj = wc.generate(' '.join(df['message'].values.tolist()))
+  return wc_obj
+
+
+def most_common_words(selected_user,df):
+  f = open('stop_hinglish.txt','r')
+  stop_words = f.read()
+  stop_words = stop_words.split()
+
+  if selected_user != 'Overall':
+    df = df[df['user'] == selected_user]
+
+  temp_df = df[df['user'] != 'group_notification']
+  media_list = ['<Media omitted>', '<\u200eimage omitted>','<\u200evideo omitted>','<\u200eGIF omitted>']
+  temp_df = temp_df[~temp_df['message'].str.strip().isin(media_list)]
+  words = []
+  for message in temp_df['message']:
+    for msg in message.lower().split():
+      if msg not in stop_words:
+        words.append(msg)
+  most_common_df = pd.DataFrame(Counter(words).most_common(10))
+  return most_common_df
